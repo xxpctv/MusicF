1fa1986d1f8000a399da749350de17867461c21a
xxpctv <105583466+xxpctv@users.noreply.github.com>
2023-02-18 13:19:41 +0800

    Add files via upload

---
+++ py/py_ck2.py
@@ -0,0 +1,24 @@
+{
+"urls": [
+{ "url": "https://神器每日推送.tk/pz.json", "name": "神器接口" }, 
+{ "url": "http://刚刚.live/猫", "name": "刚刚接口" }, 
+{ "url": "https://agit.ai/xxpctv/nzhiban/raw/branch/master/ziyong/dzb.json", "name": "⚡dz线路" },
+{"url": "https://agit.ai/Yoursmile7/TVBox/raw/branch/master/XC.json","name": "🚀南风线路"},
+{"url": "http://饭太硬.ga/x/o.json","name": "👹饭太硬"},
+{ "url": "https://agit.ai/1058263215/TVBox/raw/branch/master/JS/js0.json", "name": "道长的js0自用接口" },
+{ "url": "http://gg.gg/265aqk", "name": "2简单线路" },
+{ "url": "https://raw.liucn.cc/box/m.json", "name": "🚀老刘备线路"},
+{ "url": "https://agit.ai/66666/mao/raw/branch/master/00/000.m3u8", "name": "🐹分享者线路"},
+{ "url": "https://raw.iqiq.io/zhanghong1983/TVBOXZY/main/js.json", "name": "js接口" },
+{ "url": "https://notabug.org/oo/gao/raw/master/js.json", "name": "watson drpy1" },
+{ "url": "http://gg.gg/301aqr", "name": "简单线路" },
+{"url": "https://notabug.org/xxpctv/diy/raw/master/mao/py.py","name": "央视py"},
+{"url":"http://xn--z7x900a.love","name":"肥猫"},
+{"url": "https://agit.ai/1058263215/TVBox/raw/branch/master/🏡电视合集1.json","name": "🏡电视合集1"},
+{"url":"https://ghproxy.com/https://raw.githubusercontent.com/T00700/TVBoxSE/master/vvebo.json","name":"vvebo"}
+
+
+
+
+]
+}
