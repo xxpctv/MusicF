af6b11971e7cf97b43719c4ce842796b5523c499
xxpctv <105583466+xxpctv@users.noreply.github.com>
2023-03-26 13:10:24 +0800

    Add files via upload

--- py/py_ck.py
+++ py/py_ck.py
@@ -1,6 +1,7 @@
 {
 "urls": [
 {"url": "https://agit.ai/Yoursmile7/TVBox/raw/branch/master/XC.json","name": "南风线路"},
+{ "url": "https://agit.ai/xxpctv/m/raw/branch/master/ar/ali3", "name": "ali3线路" },
 { "url": "https://agit.ai/1058263215/TVBox/raw/branch/master/JS/js0.json", "name": "道长的js0自用接口" },
 {"url": "https://agit.ai/nbwzlyd/xiaopingguo/raw/branch/master/xiaopingguo/xiaopingguo.json","name": "小苹果线路"},
 { "url": "https://神器每日推送.tk/pz.json", "name": "神器接口" }, 
@@ -14,11 +15,20 @@
 { "url": "https://notabug.org/oo/gao/raw/master/js.json", "name": "watson drpy1" },
 { "url": "https://codeberg.org/xxpctv/ck/raw/branch/main/py/py_jr.py", "name": "简易线路" },
 {"url": "https://notabug.org/xxpctv/diy/raw/master/mao/py.py","name": "央视py"},
-{"url":"https://gitee.com/wdc558/TVBox/raw/master/YXHCh.json","name":"夕✿晨"},
 {"url":"http://我不是.肥猫.love:63/接口禁止贩卖","name":"肥猫"},
 { "url": "http://刚刚.live/猫", "name": "刚刚接口" }, 
 {"url": "https://agit.ai/1058263215/TVBox/raw/branch/master/🏡电视合集1.json","name": "电视合集1"},
 {"url":"https://ghproxy.com/https://raw.githubusercontent.com/T00700/TVBoxSE/master/vvebo.json","name":"vvebo"}
+
+
+
+
+
+
+
+
+
+
 
 
 
