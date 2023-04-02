a047938b12d4803a49221c2f48327bbb510275ec
xxpctv <105583466+xxpctv@users.noreply.github.com>
2023-04-02 11:44:21 +0800

    Add files via upload

--- py/py_ck.py
+++ py/py_ck.py
@@ -1,14 +1,13 @@
 {
 "urls": [
 {"url": "https://agit.ai/Yoursmile7/TVBox/raw/branch/master/XC.json","name": "南风线路"},
-{ "url": "https://agit.ai/xxpctv/m/raw/branch/master/ar/ali3", "name": "ali3线路" },
+{"url": "https://codeberg.org/xxpctv/ck/raw/branch/main/py/py_ali.py", "name": "ali线路" },
 { "url": "https://agit.ai/1058263215/TVBox/raw/branch/master/JS/js0.json", "name": "道长的js0自用接口" },
 {"url": "https://agit.ai/nbwzlyd/xiaopingguo/raw/branch/master/xiaopingguo/xiaopingguo.json","name": "小苹果线路"},
 { "url": "https://神器每日推送.tk/pz.json", "name": "神器接口" }, 
 { "url": "https://codeberg.org/xxpctv/ck/raw/branch/main/py/py_dz.py", "name": "pydz线路" },
 {"url": "http://饭太硬.ga/tv","name": "饭太硬"},
 {"url": "https://yydsys.top/duo","name": "大聪明家庭版"},
-{ "url": "https://codeberg.org/xxpctv/ck/raw/branch/main/py/py_ali.py", "name": "ali线路" },
 { "url": "https://raw.liucn.cc/box/m.json", "name": "老刘备线路"},
 { "url": "https://agit.ai/66666/mao/raw/branch/master/00/000.m3u8", "name": "分享者线路"},
 { "url": "https://raw.iqiq.io/zhanghong1983/TVBOXZY/main/js.json", "name": "js接口" },
