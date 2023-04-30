17cc5fb02f421b01386e09513fed6bc2a00c910b
xxpctv <105583466+xxpctv@users.noreply.github.com>
2023-04-30 13:42:10 +0800

    Add files via upload

--- py/py_ck.py
+++ py/py_ck.py
@@ -1,28 +1,24 @@
 {
 "urls": [
-{"url": "https://agit.ai/nbwzlyd/xiaopingguo/raw/branch/master/xiaopingguo/xiaopingguo.json","name": "小苹果线路"},
 {"url": "https://agit.ai/Yoursmile7/TVBox/raw/branch/master/XC.json","name": "南风线路"},
 {"url": "https://ghproxy.com/https://raw.githubusercontent.com/FongMi/CatVodSpider/main/json/config.json", "name": "FongMi" },
 {"url": "https://agit.ai/kvymin/TV/raw/branch/master/maoys.json","name": "maoys"},
 {"url":"https://agit.ai/kvymin/TV/raw/branch/master/Pandora.json","name":"Pandora"},
 {"url":"https://agit.ai/xxpctv/m/raw/branch/master/py/py_dz.py","name":"dz"},
 {"url": "https://raw.fastgit.org/FongMi/CatVodSpider/main/json/config.json","name": "蜂蜜"},
-{ "url": "https://gitee.com/lekanbox/ysc/raw/master/ysc.json", "name": "ysc" },
-{ "url": "https://agit.ai/1058263215/TVBox/raw/branch/master/JS/js0.json", "name": "js0" },
-{ "url": "https://神器每日推送.tk/pz.json", "name": "神器接口" }, 
 { "url": "https://codeberg.org/xxpctv/ck/raw/branch/main/py/drpy%20%282%29.js", "name": "drpy" },
 {"url": "https://jihulab.com/ygbh1/box/-/raw/main/%E6%9C%88%E5%85%89%E5%AE%9D%E7%9B%92", "name": "ygbh"},
 {"url": "http://饭太硬.ga/tv","name": "饭太硬"},
 {"url": "https://yydsys.top/duo","name": "duo"},
 { "url": "https://pastebin.com/raw/5NHaxyGR", "name": "js0"},
 { "url": "https://raw.iqiq.io/zhanghong1983/TVBOXZY/main/js.json", "name": "js接口" },
-{ "url": "https://notabug.org/oo/gao/raw/master/js.json", "name": "watson drpy1" },
+{ "url": "https://notabug.org/oo/gao/raw/master/js.json", "name": "js接口1" },
 {"url": "https://notabug.org/xxpctv/diy/raw/master/mao/py.py","name": "央视py"},
 {"url":"http://我不是.肥猫.love:63/接口禁止贩卖","name":"肥猫"},
 { "url": "http://刚刚.live/猫", "name": "刚刚接口" },
 { "url": "https://download.kstore.space/download/3313/mao/ymz","name": "ymz"}, 
-{ "url": "https://dybz.store/dybz", "name": "dybz" }, 
-{"url":"https://ghproxy.com/https://raw.githubusercontent.com/T00700/TVBoxSE/master/vvebo.json","name":"vvebo"}
+{ "url": "https://dybz.store/dybz", "name": "dybz" } 
+
 
 
 
