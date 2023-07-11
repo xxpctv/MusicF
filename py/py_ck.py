0bbcb518e54eb7ec777c32dee4db46922430a4c5
xxpctv <105583466+xxpctv@users.noreply.github.com>
2023-07-12 00:33:48 +0800

    Add files via upload

--- py/py_ck.py
+++ py/py_ck.py
@@ -4,20 +4,20 @@
 {"url": "https://ghproxy.com/https://raw.githubusercontent.com/FongMi/CatVodSpider/main/json/config.json", "name": "FongMi" },
 {"url": "https://agit.ai/kvymin/TV/raw/branch/master/maoys.json","name": "maoys"},
 {"url":"https://agit.ai/kvymin/TV/raw/branch/master/Pandora.json","name":"Pandora"},
-{"url":"https://agit.ai/xxpctv/m/raw/branch/master/py/py_dz.py","name":"dz"},
+{"url":"https://codeberg.org/xxpctv/n/raw/branch/main/py/py_xc.py","name":"dz"},
 {"url": "https://raw.fastgit.org/FongMi/CatVodSpider/main/json/config.json","name": "蜂蜜"},
-{ "url": "https://codeberg.org/xxpctv/ck/raw/branch/main/py/drpy%20%282%29.js", "name": "drpy" },
+{"url": "https://codeberg.org/xxpctv/ck/raw/branch/main/py/drpy%20%282%29.js", "name": "drpy" },
 {"url": "https://jihulab.com/ygbh1/box/-/raw/main/%E6%9C%88%E5%85%89%E5%AE%9D%E7%9B%92", "name": "ygbh"},
-{"url": "http://饭太硬.ga/tv","name": "饭太硬"},
+{"url": "http://饭太硬.ml/tv","name": "饭太硬"},
 {"url": "https://yydsys.top/duo","name": "duo"},
-{ "url": "https://pastebin.com/raw/5NHaxyGR", "name": "js0"},
-{ "url": "https://raw.iqiq.io/zhanghong1983/TVBOXZY/main/js.json", "name": "js接口" },
-{ "url": "https://notabug.org/oo/gao/raw/master/js.json", "name": "js接口1" },
+{"url": "https://pastebin.com/raw/5NHaxyGR", "name": "js0"},
+{"url": "https://raw.iqiq.io/zhanghong1983/TVBOXZY/main/js.json", "name": "js接口" },
+{"url": "https://notabug.org/oo/gao/raw/master/js.json", "name": "js接口1" },
 {"url": "https://notabug.org/xxpctv/diy/raw/master/mao/py.py","name": "央视py"},
 {"url":"http://我不是.肥猫.love:63/接口禁止贩卖","name":"肥猫"},
-{ "url": "http://刚刚.live/猫", "name": "刚刚接口" },
-{ "url": "https://download.kstore.space/download/3313/mao/ymz","name": "ymz"}, 
-{ "url": "https://dybz.store/dybz", "name": "dybz" } 
+{"url": "http://刚刚.live/猫", "name": "刚刚接口" },
+{"url": "https://download.kstore.space/download/3313/mao/ymz","name": "ymz"},
+{"url": "https://dybz.store/dybz", "name": "dybz" } 
 
 
 
