bd94778aa43e3fff0166f809efd21e2d14156b36
xxpctv <105583466+xxpctv@users.noreply.github.com>
2023-11-16 14:02:33 +0800

    Add files via upload

--- py/py_ck.py
+++ py/py_ck.py
@@ -1,23 +1,20 @@
 {
 "urls": [
 {"url": "https://agit.ai/Yoursmile7/TVBox/raw/branch/master/XC.json","name": "南风线路"},
-{"url": "https://ghproxy.com/https://raw.githubusercontent.com/FongMi/CatVodSpider/main/json/config.json", "name": "FongMi" },
-{"url": "https://agit.ai/kvymin/TV/raw/branch/master/maoys.json","name": "maoys"},
+{"url": "https://ghproxy.net/https://raw.githubusercontent.com/FongMi/CatVodSpider/main/json/config.json", "name": "FongMi" },
+{"url": "https://agit.ai/kvymin/TV/raw/branch/master/JavaScript.json","name": "js"},
 {"url":"https://agit.ai/kvymin/TV/raw/branch/master/Pandora.json","name":"Pandora"},
-{"url":"https://codeberg.org/xxpctv/n/raw/branch/main/py/py_xc.py","name":"dz"},
+{"url":"https://codeberg.org/xxpctv/n/raw/branch/main/py/py_xc.py","name":"xc"},
 {"url": "https://raw.fastgit.org/FongMi/CatVodSpider/main/json/config.json","name": "蜂蜜"},
 {"url": "https://codeberg.org/xxpctv/ck/raw/branch/main/py/drpy%20%282%29.js", "name": "drpy" },
-{"url": "https://jihulab.com/ygbh1/box/-/raw/main/%E6%9C%88%E5%85%89%E5%AE%9D%E7%9B%92", "name": "ygbh"},
-{"url": "http://饭太硬.ml/tv","name": "饭太硬"},
+{"url": "https://agit.ai/zhoujck/config/raw/branch/master/box", "name": "box"},
+{"url": "http://饭太硬.top/tv","name": "饭太硬"},
 {"url": "https://yydsys.top/duo","name": "duo"},
 {"url": "https://pastebin.com/raw/5NHaxyGR", "name": "js0"},
-{"url": "https://raw.iqiq.io/zhanghong1983/TVBOXZY/main/js.json", "name": "js接口" },
-{"url": "https://notabug.org/oo/gao/raw/master/js.json", "name": "js接口1" },
+{"url": "https://agit.ai/lanhaidixingren/Tvbox/raw/branch/master/py.json", "name": "py接口1" },
 {"url": "https://notabug.org/xxpctv/diy/raw/master/mao/py.py","name": "央视py"},
 {"url":"http://我不是.肥猫.love:63/接口禁止贩卖","name":"肥猫"},
-{"url": "http://刚刚.live/猫", "name": "刚刚接口" },
-{"url": "https://download.kstore.space/download/3313/mao/ymz","name": "ymz"},
-{"url": "https://dybz.store/dybz", "name": "dybz" } 
+{"url": "http://刚刚.live/猫", "name": "刚刚接口" }
 
 
 
