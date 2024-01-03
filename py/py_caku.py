66bdd56687d0066e0be7071bed34e6dbd43e9bda
xxpctv <105583466+xxpctv@users.noreply.github.com>
2024-01-03 13:49:22 +0800

    Add files via upload

---
+++ py/py_caku.py
@@ -0,0 +1,1 @@
+{"storeHouse": [{"sourceName":"花满城","sourceUrl":"https://ghproxy.net/https://raw.githubusercontent.com/xxpctv/drjs/main/js/drck.js"}]}
---
+++ py/py_ku2.py
@@ -0,0 +1,1 @@
+{"storeHouse": [{"sourceName":"春天满花城","sourceUrl":"https://ghproxy.net/https://raw.githubusercontent.com/xxpctv/MusicF/main/py/py_ck.py"}]}
