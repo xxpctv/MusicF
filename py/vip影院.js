c97f1bfdef08b274409b3f4a3a72647794abd884
xxpctv <105583466+xxpctv@users.noreply.github.com>
2024-03-08 11:46:48 +0800

    Add files via upload

---
+++ py/vip影院.js
@@ -0,0 +1,26 @@
+﻿var rule = {
+    title:'s VIP影院',
+    host:'https://www.mfyy8.com/',
+    // homeUrl:'/',
+    url:'index.php/vod/show/id/23/page/2.html
+    searchUrl:'index.php/vod/search/page/fypage/wd/**.html
+    searchable:2,//是否启用全局搜索,
+    quickSearch:1,//是否启用快速搜索,
+    filterable:0,//是否启用分类筛选,
+    headers:{//网站的请求头,完整支持所有的,常带ua和cookies
+        'User-Agent':'MOBILE_UA',
+        // "Cookie": "searchneed=ok"
+    },
+    class_name:'电影&连续剧&综艺&动漫&直播',
+    class_url:'1&2&3&4&20&',
+    play_parse:true,
+    lazy:'',
+    limit:6,
+    推荐:'*',
+    //推荐:'.module-item;.module-item-cover&&.module-item-pic;a&&title;.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
+    double:true, // 推荐内容是否双层定位
+    //一级:'.module-list&&.module-item-pic;a&&title;.lazyloaded&&data-src;.module-item-text&&Text;a&&href',
+    一级:'.hl-list-wrap li;a&&title;.hl-lazy&&data-original;.remarks&&Text;a&&href',
+    二级:{"title":"h2.hl-dc-title hl-data-menu&&Text;.video-info-aux&&Text","img":".lazyload&&data-src","desc":".hl-col-xs-12 hl-col-sm-4:eq(-1)&&Text;.hl-col-xs-12 hl-col-sm-4:eq(-3)&&Text;.hl-col-xs-12 hl-col-sm-4:eq(-2)&&Text;.hl-col-xs-12 hl-col-sm-4:eq(1)&&.hl-col-xs-12 hl-col-sm-4&&Text;.video-info-items:eq(0)&&.video-info-item.video-info-actor&&Text","content":".hl-col-xs-12 .blurb&&Text","tabs":".hl-from-list","lists":".hl-plays-list:eq(#id) a"},
+    搜索:'.hl-list-wrap;.hl-item-title&&Text;.hl-item-thumb.hl-lazy&&data-original;.hl-pic-text&&Text;a&&href',
+ }
